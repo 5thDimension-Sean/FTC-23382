c39658dede677fea2f7079f950b5c5603ad38ce4
5thDimension-Sean <ss0486034@gmail.com>
2026-07-25 11:03:17 -0400

    fix pid and more constants
    Signed-off-by: 5thDimension-Sean <ss0486034@gmail.com>

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
@@ -22,25 +22,25 @@ public class FConstants {
     }
 
     static {
-        followerConstants.mass = 12.0;
+        followerConstants.mass = 8;
         followerConstants.forwardZeroPowerAcceleration = -30.01;
         followerConstants.lateralZeroPowerAcceleration = -85.11;
 
         // Translational PID settings
-        followerConstants.coefficientsTranslationalPIDF.setCoefficients(0.1, 0, 0.01, 0);
+        followerConstants.coefficientsTranslationalPIDF.setCoefficients(0.08, 0, 0.02, 0);
         // Heading PID settings
-        followerConstants.coefficientsHeadingPIDF.setCoefficients(2, 0, 0.1, 0);
+        followerConstants.coefficientsHeadingPIDF.setCoefficients(1.5, 0, 0.2, 0);
         // Drive PID settings
-        followerConstants.coefficientsDrivePIDF.setCoefficients(0.1, 0, 0, 0.6, 0);
+        followerConstants.coefficientsDrivePIDF.setCoefficients(0.05, 0, 0.01, 0.6, 0);
 
         mecanumConstants.leftFrontMotorName = "frontleft";
         mecanumConstants.leftRearMotorName = "backleft";
         mecanumConstants.rightFrontMotorName = "frontright";
         mecanumConstants.rightRearMotorName = "backright";
 
-        mecanumConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
-        mecanumConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
-        mecanumConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
-        mecanumConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+        mecanumConstants.leftFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
+        mecanumConstants.leftRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+        mecanumConstants.rightFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
+        mecanumConstants.rightRearMotorDirection = DcMotorSimple.Direction.REVERSE;
     }
 }
