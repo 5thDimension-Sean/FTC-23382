fd2d94cfb4793c6bdc93886b343d41db3587f170
5thDimension-Sean <ss0486034@gmail.com>
2026-08-01 10:35:11 -0400

    fixing bugs
    Signed-off-by: 5thDimension-Sean <ss0486034@gmail.com>

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -24,7 +24,5 @@ public class LConstants {
         driveEncoderConstants.rightFrontEncoderDirection = Encoder.FORWARD;
         driveEncoderConstants.leftRearEncoderDirection = Encoder.FORWARD;
         driveEncoderConstants.rightRearEncoderDirection = Encoder.FORWARD;
-
-        driveEncoderConstants.forwardTicksToInches(5.94);
     }
 }
