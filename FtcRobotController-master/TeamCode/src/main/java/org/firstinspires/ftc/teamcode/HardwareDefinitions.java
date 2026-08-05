bae6db564c4c61d296bb7e7d9a91f61cbcdef6ac
5thDimension-Sean <ss0486034@gmail.com>
2026-08-04 20:07:40 -0400

    fix issues in gradle causing errors
    Signed-off-by: 5thDimension-Sean <ss0486034@gmail.com>

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareDefinitions.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareDefinitions.java
@@ -6,76 +6,45 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.TouchSensor;
+
 public class HardwareDefinitions {
     public DcMotor fl = null;
     public DcMotor fr = null;
     public DcMotor bl = null;
     public DcMotor br = null;
     public IMU imu = null;
-    public DistanceSensor frontDistance;
-    public DcMotorEx parallel;
-    public DcMotorEx perpendicular;
-    public ColorSensor colorSensor;
-    public TouchSensor limitSwitch;
 
     public void init(HardwareMap hardwareMap, Telemetry telemetry) {
-        // Motor configuration
-        fl = hardwareMap.get(DcMotor.class, "frontleft");
-        fr = hardwareMap.get(DcMotor.class, "frontright");
-        bl = hardwareMap.get(DcMotor.class, "backleft");
-        br = hardwareMap.get(DcMotor.class, "backright");
+        try {
+            // Motor configuration
+            fl = hardwareMap.get(DcMotor.class, "frontleft");
+            fr = hardwareMap.get(DcMotor.class, "frontright");
+            bl = hardwareMap.get(DcMotor.class, "backleft");
+            br = hardwareMap.get(DcMotor.class, "backright");
 
-        fl.setDirection(DcMotorSimple.Direction.REVERSE);
-        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+            fl.setDirection(DcMotorSimple.Direction.REVERSE);
+            bl.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        fr.setZeroPowerBehavior(FLOAT);
-        fl.setZeroPowerBehavior(FLOAT);
-        bl.setZeroPowerBehavior(FLOAT);
-        br.setZeroPowerBehavior(FLOAT);
+            fl.setZeroPowerBehavior(FLOAT);
+            fr.setZeroPowerBehavior(FLOAT);
+            bl.setZeroPowerBehavior(FLOAT);
+            br.setZeroPowerBehavior(FLOAT);
 
-        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        telemetry.addLine("Motors Initialized");
-        telemetry.update();
+            // IMU configuration
+            imu = hardwareMap.get(IMU.class, "imu");
 
-        // IMU configuration
-
-        imu = hardwareMap.get(IMU.class, "imu");
-        IMU.Parameters parameters = new IMU.Parameters(
-                new RevHubOrientationOnRobot(
-                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                        RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
-                )
-        );
-        imu.initialize(parameters);
-        telemetry.addLine("IMU Initialized");
-        telemetry.update();
-
-        // Odometry configuration
-        //parallel = hardwareMap.get(DcMotorEx.class, "fl");
-        //perpendicular = hardwareMap.get(DcMotorEx.class, "fr");
-
-        //parallel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //perpendicular.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //parallel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        //perpendicular.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        telemetry.addLine("Odometry Initialized");
-        telemetry.update();
-
-        //frontDistance = hardwareMap.get(DistanceSensor.class, "frDist");
-        //colorSensor = hardwareMap.get(ColorSensor.class, "color_sensor");
-        //limitSwitch = hardwareMap.get(TouchSensor.class, "limit_switch");
-        telemetry.addLine("Sensors Initialized");
+            telemetry.addLine("Hardware Init Sequence Complete");
+        } catch (Exception e) {
+            telemetry.addLine("Init Warning: Some hardware not found");
+        }
         telemetry.update();
     }
 }
